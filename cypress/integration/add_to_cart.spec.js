8b0f4c77ba8734f21a97f1a49f9083b88aba6c6c
Kurt Maglalang <kbm126@live.com>
2022-05-10 18:52:47 -0700

    add another test for add to cart cypress test

--- cypress/integration/add_to_cart.spec.js
+++ cypress/integration/add_to_cart.spec.js
@@ -9,8 +9,14 @@ describe('Add to Cart', () => {
   });
 
   it("See cart count increases after clicking the add button", () => {
-    // cy.get('.products article').get('.button_to').first().submit()
-    cy.get('.products article:nth-child(1) > div > form').submit()
+    cy.get('.products article').get('.button_to').first().submit()
+    // cy.get('.products article:nth-child(1) > div > form').submit()
+    cy.get('a[href*="/cart"]').should('contain', 'My Cart (1)')
+  });
+
+  it("See cart count doesn't change after clicking the add button for sold out product", () => {
+    cy.get('.products article').get('.button_to').last().submit()
+    // cy.get('.products article:nth-child(2) > div > form').submit()
     cy.get('a[href*="/cart"]').should('contain', 'My Cart (1)')
   });
 })
