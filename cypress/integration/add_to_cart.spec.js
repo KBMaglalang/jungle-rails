9dda97964b848166b8c82c199c200dd478d6de06
Kurt Maglalang <kbm126@live.com>
2022-05-10 17:03:31 -0700

    change out the form get to use selectors

--- cypress/integration/add_to_cart.spec.js
+++ cypress/integration/add_to_cart.spec.js
@@ -9,7 +9,8 @@ describe('Add to Cart', () => {
   });
 
   it("See cart count increases after clicking the add button", () => {
-    cy.get('.products article').get('.button_to').first().submit()
+    // cy.get('.products article').get('.button_to').first().submit()
+    cy.get('.products article:nth-child(1) > div > form').submit()
     cy.get('a[href*="/cart"]').should('contain', 'My Cart (1)')
   });
 })
